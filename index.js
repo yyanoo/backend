ec4711be8a70f7268f281564a3a0922bee09aef3
yyanoo <junhong07245@gmail.com>
2025-06-12 06:02:12 +0900

    update

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { getCardData } from "./models/models.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.port;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express ES Module!");
+app.get("/cards/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await getCardData(id);
+    res.json(data);
+  } catch (error) {
+    console.error("抓資料出錯：", error);
+    res.status(500).json({ error: "伺服器錯誤" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
