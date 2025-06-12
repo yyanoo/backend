93abb46ddfd20e4c60c0d67de9a9cd63dec95fc7
yyanoo <junhong07245@gmail.com>
2025-06-12 20:59:40 +0900

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 const port = process.env.port;
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/cards", routes);
