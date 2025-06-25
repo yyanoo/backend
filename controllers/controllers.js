b53f43df1f5da0fb0334d1bd273ce67f986d7277
yyanoo <junhong07245@gmail.com>
2025-06-26 05:14:22 +0900

    update

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,31 +1,38 @@
-import getCardData from "../models/models.js";
 import getAllCard from "../models/allCard.js";
-
-export async function getCardById(req, res) {
-  const id = req.params.id;
-  try {
-    const data = await getCardData(id);
-
-    if (!data || data.length === 0) {
-      return res.status(404).json({ message: "找不到資料" });
-    } else {
-      res.status(200).json(data);
-    }
-  } catch (err) {
-    console.error("取得卡片資料錯誤：", err);
-    res.status(500).json({ message: "伺服器錯誤" });
-  }
-}
 
 export async function allCard(req, res) {
   try {
-    const data = await getAllCard();
+    const { id, title, color, lvl } = req.query; // 從 URL query 拿 id 和 title
+
+    const data = await getAllCard(); // 取得全部資料（陣列）
 
     if (!data || data.length === 0) {
       return res.status(404).json({ message: "找不到資料" });
-    } else {
-      res.status(200).json(data);
     }
+
+    let result = data;
+
+    if (title) {
+      result = result.filter((item) => item.title?.includes(title));
+    }
+
+    if (id) {
+      result = result.filter((item) => item.id.includes(id));
+    }
+
+    if (color) {
+      result = result.filter((item) => item.color.includes(color));
+    }
+
+    if (lvl) {
+      result = result.filter((item) => item.lvl.includes(lvl));
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "找不到符合條件的資料" });
+    }
+
+    return res.status(200).json(result);
   } catch (err) {
     console.error("取得卡片資料錯誤：", err);
     res.status(500).json({ message: "伺服器錯誤" });
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,8 @@
 import express from 'express'
-import { getCardById } from '../controllers/controllers.js'
 import { allCard } from '../controllers/controllers.js'
 
 const router = express.Router()
 
-router.get('/:id', getCardById)
 router.get('/', allCard)
 
 export default router
